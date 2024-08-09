9baf5c768b735ecf82e87a7211e10123d82934b3
kimchanjun <rlackswns7@gmail.com>
2024-08-09 16:26:01 +0900

    add Mapper

--- src/main/java/com/springboot/mapper/TodoMapper.java
+++ src/main/java/com/springboot/mapper/TodoMapper.java
@@ -1,16 +1,19 @@
-package com.mapper;
+package com.springboot.mapper;
 
 
-import com.dto.TodoPatchDto;
-import com.dto.TodoPostDto;
-import com.dto.TodoResponseDto;
-import com.entity.Todos;
+import com.springboot.dto.TodoPatchDto;
+import com.springboot.dto.TodoPostDto;
+import com.springboot.dto.TodoResponseDto;
+import com.springboot.entity.Todos;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TodoMapper {
   Todos todosPostDtoToTodos(TodoPostDto todoPostDto);
   Todos todosPatchDtoToTodos(TodoPatchDto todoPatchDto);
   TodoResponseDto todosToTodoResponseDto(Todos todos);
+  List<TodoResponseDto> todosToTodoResponseDtos(List<Todos> todos);
 
 }
