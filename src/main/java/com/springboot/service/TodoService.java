b600754fe3525a88cb402619dc2b1f4f0e79e0e4
kimchanjun <rlackswns7@gmail.com>
2024-08-09 16:27:27 +0900

    add Service

--- src/main/java/com/springboot/service/TodoService.java
+++ src/main/java/com/springboot/service/TodoService.java
@@ -1,13 +1,64 @@
-package com.service;
+package com.springboot.service;
 
-import com.entity.Todos;
+import com.springboot.entity.Todos;
+import com.springboot.repository.TodoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
-  public Todos createTodo(Todos todos) {
-    Todos createdTodo = todos;
+  private final TodoRepository todoRepository;
 
-    return createdTodo;
+  public TodoService(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
   }
+
+  public Todos createTodo(Todos todos) {
+    return todoRepository.save(todos);
+  }
+
+
+  public Todos updateTodo(Todos todos) {
+    Todos findTodo = findVerifiedTodo(todos.getId());
+
+    Optional.ofNullable(todos.getTitle())
+        .ifPresent(title -> findTodo.setTitle(title));
+    Optional.ofNullable(todos.getTodoOrder())
+        .ifPresent(order -> findTodo.setTodoOrder(order));
+    boolean completed = todos.isCompleted();
+    findTodo.setCompleted(completed);
+
+    return todoRepository.save(findTodo);
+  }
+  public Todos findTodo(long id) {
+    return findVerifiedTodo(id);
+  }
+  public List<Todos> findAlltodos(){
+    return todoRepository.findAll();
+  }
+  //페이징 네이션 서비스
+//  public Page<Todos> findTodos(int page, int size) {
+//    return todoRepository.findAll(PageRequest.of(page, size, Sort.by("id").descending()));
+//  }
+
+
+  public void deleteTodo(long id) {
+    Todos findTodo = findVerifiedTodo(id);
+
+    todoRepository.delete(findTodo);
+
+  }
+  public void deleteAllTodos() {
+    todoRepository.deleteAll();
+  }
+
+  public Todos findVerifiedTodo(long id) {
+    Optional<Todos> optionalTodos = todoRepository.findById(id);
+    Todos findTodos =
+        optionalTodos.orElseThrow(() -> new RuntimeException("Todo not found"));
+    return findTodos;
+  }
+
 }
