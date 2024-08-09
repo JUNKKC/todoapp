810b598ba7a89f1331d69add06d00f452774473e
kimchanjun <rlackswns7@gmail.com>
2024-08-09 16:27:02 +0900

    add Repository

--- src/main/java/com/springboot/repository/TodoRepository.java
+++ src/main/java/com/springboot/repository/TodoRepository.java
@@ -1,2 +1,10 @@
-package com.springboot.repository;public class TodoRepository {
+package com.springboot.repository;
+
+import com.springboot.entity.Todos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TodoRepository extends JpaRepository<Todos, Long> {
+  Optional<Todos> findByTitle(String title);
 }
