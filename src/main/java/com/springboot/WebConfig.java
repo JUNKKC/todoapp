9a326bff27878bcc1d7e380de9852e20b9b1425e
kimchanjun <rlackswns7@gmail.com>
2024-08-12 11:37:25 +0900

    CORS 해결 실패

--- src/main/java/com/springboot/WebConfig.java
+++ src/main/java/com/springboot/WebConfig.java
@@ -5,14 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//        .allowedOriginPatterns("*") // 허용할 도메인
+//        .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+//        .allowCredentials(true);
+//  }
+//}
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOriginPatterns("*") // 허용할 도메인
-        .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
-        .allowCredentials(true);
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")
+        .allowedHeaders("Content-Type", "Authorization", "Accept")
+        .allowCredentials(true)
+        .maxAge(3000);
   }
 }
