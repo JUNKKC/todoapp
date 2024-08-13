8a7718d041761354f4a36c29ef4786332213d75b
kimchanjun <rlackswns7@gmail.com>
2024-08-13 16:29:34 +0900

    주석 처리

--- src/main/java/com/springboot/response/SingleResponseDto.java
+++ src/main/java/com/springboot/response/SingleResponseDto.java
@@ -1,10 +1,11 @@
-package com.springboot.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class SingleResponseDto<T> {
-  private T data;
-}
+//package com.springboot.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//
+//@AllArgsConstructor
+//@Getter
+//public class SingleResponseDto<T> {
+//  private T data;
+//}
+// 아직 필요없는 기능임
