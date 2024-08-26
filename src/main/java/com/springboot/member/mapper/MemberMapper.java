0c9910ba1f13ff91f60555f316843cbd6318e27c
kimchanjun <rlackswns7@gmail.com>
2024-08-26 17:44:28 +0900

    unmappedTargetPolicy = ReportingPolicy.IGNORE)코드 추가

--- src/main/java/com/springboot/member/mapper/MemberMapper.java
+++ src/main/java/com/springboot/member/mapper/MemberMapper.java
@@ -5,10 +5,11 @@ import com.springboot.member.dto.MemberPostDto;
 import com.springboot.member.dto.MemberResponseDto;
 import com.springboot.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
 Member memberPostDtoToMember(MemberPostDto memberPostDto);
 Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
