851d45caf0bcb203bcbbd92d43b5023d72e79a4d
kimchanjun <rlackswns7@gmail.com>
2024-08-28 11:14:13 +0900

    “사용자 기반 Todo 검색 기능 추가 및 대소문자 구분 없는 검색 확장.”

--- src/main/java/com/springboot/todos/repository/TodoRepository.java
+++ src/main/java/com/springboot/todos/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.springboot.todos.repository;
 
 import com.springboot.todos.entity.Todos;
+import com.springboot.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todos, Long> {
   Optional<Todos> findByTitle(String title);
-
-// 제목을 대소문자 구분 없이 검색
-List<Todos> findByTitleContainingIgnoreCase(String title);
+  List<Todos> findByTitleContainingIgnoreCase(String title);
+  List<Todos> findByMember(Member member);
+  List<Todos> findByMemberAndTitleContainingIgnoreCase(Member member, String title);
 }
